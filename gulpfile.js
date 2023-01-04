ac9eb8457789854d3c5d7a753e3cfa0521d9cee9
checkthesound <danil.dybko@gmail.com>
2023-01-05 02:31:10 +0300

    go to normal

--- gulpfile.js
+++ gulpfile.js
@@ -6,22 +6,38 @@ var fileinclude = require('gulp-file-include');
 var replace = require('gulp-replace');
 var webpHtmlNoSvg = require('gulp-webp-html-nosvg');
 
-function deleteall(done){
+function deleteall(done) {
     del('./dist/**/*');
 
     done();
 }
 
-function html(done)
-{
+function html(done) {
     gulp.src('./src/**.html')
         .pipe(gulp.dest('./dist'));
 
     done();
 }
 
+function getcss(done) {
+    gulp.src('./src/css/**/*.css')
+        .pipe(gulp.dest('./dist/css'));
+
+    done();
+}
+
+function javascript(done) {
+    gulp.src('./src/js/**/*.js')
+        .pipe(gulp.dest('./dist/js'));
+
+    done();
+}
 
 gulp.task('default', gulp.series(
     deleteall,
     html,
+    getcss,
+    javascript
     ));
+
+gulp.task(getcss);
