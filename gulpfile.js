ca24451b7bfb545e4d8670ad03d143b1d604d6d5
checkthesound <danil.dybko@gmail.com>
2023-01-05 02:51:34 +0300

    work with css

--- gulpfile.js
+++ gulpfile.js
@@ -6,17 +6,20 @@ var fileinclude = require('gulp-file-include');
 var replace = require('gulp-replace');
 var webpHtmlNoSvg = require('gulp-webp-html-nosvg');
 
-function deleteall(done) {
-    del('./dist/**/*');
+var cssmin = require('gulp-cssmin');
+var csso = require('gulp-csso');
+var concatCss = require('gulp-concat-css');
+var autoprefixer = require('gulp-autoprefixer');
 
-    done();
-}
+// function deleteall(done) {
+//     del('./dist/**/*');
+
+//     done();
+// }
 
 function html(done) {
     gulp.src('./src/**.html')
         .pipe(fileinclude())
-        .pipe(replace(/@img\//g, 'img/'))
-        .pipe(webpHtmlNoSvg())
         .pipe(replace(/@img\//g, 'img/'))
         .pipe(webpHtmlNoSvg())
         .pipe(htmlmin({
@@ -30,6 +33,10 @@ function html(done) {
 
 function getcss(done) {
     gulp.src('./src/css/**/*.css')
+        .pipe(autoprefixer())
+        .pipe(csso())
+        .pipe(cssmin())
+        .pipe(concatCss('index.css'))
         .pipe(gulp.dest('./dist/css'));
 
     done();
@@ -49,18 +56,39 @@ function javascript(done) {
     done();
 }
 
-function fonts(done) {
+function compressFonts(done) {
     gulp.src('./src/fonts/**/*.{woff2,woff,otf,ttf}')
         .pipe(gulp.dest('./dist/fonts'));
 
     done();
 }
 
-gulp.task('default', gulp.series(
-    // deleteall,
+function compressImages(done) {
+    gulp.src('./src/img/**/*.{jpg, webp, svg, avi}')
+        .pipe(gulp.dest('./dist/img'));
+
+    done();
+}
+
+gulp.task('build', gulp.series(
+    html,
+    getcss,
+    images,
+    javascript
+));
+
+gulp.task('compress', gulp.series(
+    html,
+    getcss,
+    compressImages,
+    compressFonts,
+    javascript
+));
+
+gulp.task('serve', gulp.series(
     html,
     getcss,
     images,
     javascript,
     fonts
-    ));
+));
