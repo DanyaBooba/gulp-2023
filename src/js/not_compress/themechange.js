f6063bc0759f54403f37da4b8eb3f32bcc062e17
checkthesound <danil.dybko@gmail.com>
2023-01-07 01:07:31 +0300

    work at dark mode

--- src/js/not_compress/themechange.js
+++ src/js/not_compress/themechange.js
@@ -1,89 +1,50 @@
-let button = document.getElementById('themechangebutton');
 let btnicon = document.getElementById('themechangebutton_icon');
-let btnAuto = document.getElementById('changethemeauto');
-
 let light = document.getElementById('stylesheetlight');
 let dark = document.getElementById('stylesheetdark');
 
-function ButtonAuto() {
-    MainControl('auto');
-}
-
-function ButtonLight() {
-    MainControl('light');
-}
-
-function ButtonDark() {
-    MainControl('dark');
-}
-
-function MainControl(theme) {
-    if (theme === "light") {
-        ChangeStatusButtonLight();
-        ChangeStatusLight();
-    } else if (theme === "dark") {
-        ChangeStatusButtonDark();
-        ChangeStatusDark();
-    } else {
-        ChangeStatusButtonAuto();
-        ChangeStatusAuto();
+function ThemeChangeButton() {
+    let theme = localStorage.getItem('color-theme');
+    if (theme === 'light') {
+        GetAuto();
+    }
+    else if (theme === 'dark') {
+        GetLight();
+    }
+    else if (theme !== 'dark' && theme !== 'light') {
+        GetDark();
+    }
+    else {
+        GetAuto();
     }
 }
 
-function ChangeStatusAuto() {
-    localStorage.removeItem('color-theme');
-    light.media = "(prefers-color-scheme: light)";
-    dark.media = "(prefers-color-scheme: dark)";
-}
-
-function ChangeStatusButtonDark() {
-    ClearAllClasses();
-
-    if(btnDark !== null) {btnDark.classList.add('btn-dark');}
-    if (btnLight !== null) {btnLight.classList.add('btn-light');}
-    if (btnAuto !== null) {btnAuto.classList.add('btn-light');}
-}
-
-function ChangeStatusLight() {
-    localStorage.setItem('color-theme', 'light');
-    light.media = "all";
-    dark.media = "not all";
-}
-
-function ChangeStatusButtonLight() {
-    ClearAllClasses();
-
-    if (btnLight !== null) {btnLight.classList.add('btn-dark');}
-    if(btnDark !== null) {btnDark.classList.add('btn-light');}
-    if(btnAuto !== null) {btnAuto.classList.add('btn-light');}
-}
-
-function ChangeStatusDark() {
+function GetDark() {
+    if(btnicon !== null) { btnicon.href.baseVal = "/img/icons/themes.svg#dark"; }
     localStorage.setItem('color-theme', 'dark');
     light.media = "not all";
     dark.media = "all";
 }
 
-function ChangeStatusButtonAuto() {
-    ClearAllClasses();
-
-    if(btnAuto !== null) {btnAuto.classList.add('btn-dark');}
-    if(btnDark !== null) {btnDark.classList.add('btn-light');}
-    if(btnLight !== null) {btnLight.classList.add('btn-light');}
+function GetLight() {
+    if(btnicon !== null) { btnicon.href.baseVal = "/img/icons/themes.svg#light"; }
+    localStorage.setItem('color-theme', 'light');
+    dark.media = "not all";
+    light.media = "all";
 }
 
-function ClearAllClasses() {
-    if(btnDark !== null) {btnDark.classList.remove('btn-dark');}
-    if(btnDark !== null) {btnDark.classList.remove('btn-light');}
-    if(btnLight !== null) {btnLight.classList.remove('btn-dark');}
-    if(btnLight !== null) {btnLight.classList.remove('btn-light');}
-    if(btnAuto !== null) {btnAuto.classList.remove('btn-dark');}
-    if(btnAuto !== null) {btnAuto.classList.remove('btn-light');}
+function GetAuto() {
+    if(btnicon !== null) { btnicon.href.baseVal = "/img/icons/themes.svg#auto"; }
+    localStorage.removeItem('color-theme');
+    light.media = "(prefers-color-scheme: light)";
+    dark.media = "(prefers-color-scheme: dark)";
 }
 
-if (localStorage.getItem('color-theme')) {
-    MainControl(localStorage.getItem('color-theme'));
+if (localStorage.getItem('color-theme') === 'dark') {
+    GetDark();
+}
+else if (localStorage.getItem('color-theme') === 'light') {
+    GetLight();
 }
 else {
-    MainControl('auto');
+    GetAuto();
 }
