749f50b0a9741f9b5a46be2ac8bd32b1de91f8ce
checkthesound <danil.dybko@gmail.com>
2023-01-06 07:15:25 +0300

    work at game page

--- src/_js/game-page.js
+++ src/_js/game-page.js
@@ -1,1 +1,19 @@
-console.log('123');
+function showText() {
+    let media = document.getElementById('divMedia');
+    let text = document.getElementById('divText');
+
+    if (media === null || text === null) return;
+
+    media.classList.add('d-none');
+    text.classList.remove('d-none');
+}
+
+function showMedia() {
+    let media = document.getElementById('divMedia');
+    let text = document.getElementById('divText');
+
+    if (media === null || text === null) return;
+
+    media.classList.remove('d-none');
+    text.classList.add('d-none');
+}
