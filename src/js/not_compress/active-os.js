53cafc956e6649d88502d81d4ac7a372d926f315
checkthesound <danil.dybko@gmail.com>
2023-01-10 07:28:26 +0300

    start to work at active os

--- src/js/not_compress/active-os.js
+++ src/js/not_compress/active-os.js
@@ -1,1 +1,25 @@
-console.log('os');
+let MACOS = navigator.appVersion.indexOf('OS X');
+let WINDOWS = navigator.appVersion.indexOf('Windows');
+let ANDROID = navigator.appVersion.indexOf('Android');
+let LINUX = navigator.appVersion.indexOf('Linux');
+
+if (MACOS > -1) { GetMacOS(); }
+if (WINDOWS > -1) { GetWindows(); }
+if (ANDROID > -1) { GetAndroid(); }
+if (LINUX > -1) { GetLinux(); }
+
+function GetMacOS() {
+    console.log('macos');
+}
+
+function GetWindows() {
+    console.log('windows');
+}
+
+function GetAndroid() {
+    console.log('android');
+}
+
+function GetLinux() {
+    console.log('linux');
+}
