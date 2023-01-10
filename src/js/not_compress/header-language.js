24eb5363f34c69075ee42890aeaf0eaec3716e05
checkthesound <danil.dybko@gmail.com>
2023-01-10 07:05:42 +0300

    add js to change language block

---
+++ src/js/not_compress/header-language.js
@@ -0,0 +1,21 @@
+let langUser = navigator.language;
+
+if (langUser !== "ru-RU" || langUser !== "be_BY" || langUser !== "ce_RU" || langUser !== "uk_UA") {
+    ShowChangeLanguage();
+}
+
+function ShowChangeLanguage() {
+    let btnClose = localStorage.getItem('lang-close');
+
+    if (btnClose !== 'close') {
+        let mainblock = document.getElementById('changelanguage__mainblock');
+        if(mainblock !== null) { mainblock.classList.remove('d-none'); }
+    }
+}
+
+function CloseHeaderLanguage() {
+    localStorage.setItem('lang-close', 'close');
+
+    let mainblock = document.getElementById('changelanguage__mainblock');
+    if(mainblock !== null) { mainblock.classList.add('d-none'); }
+}
